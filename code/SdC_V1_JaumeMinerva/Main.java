abd4b6ee315ee2c9b6b3e7ee4da04ff2032ba898
JaumeMinerva <91249362+JaumeMinerva@users.noreply.github.com>
2021-12-16 12:47:48 +0100

    Create Main.java

---
+++ code/SdC_V1_JaumeMinerva/Main.java
@@ -0,0 +1,29 @@
+
+package com.iesebre;
+
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        boolean finish = false;
+        do {
+            Scanner scanner = new Scanner(System.in);
+
+            // Escollir amb qui volem conversar
+            System.out.println("Amb qui vols conversar?");
+            System.out.println(" - Persona");
+            System.out.println(" - Animal");
+            System.out.print("Opció: ");
+            switch (scanner.nextLine()) {
+                case "Persona":
+                    Person person = new Person();
+                    finish = person.talking();
+                    break;
+                case "Animal":
+                    System.out.println("Esta en proces..");
+                    /*Animal animal = new Animal();
+                    finish = animal.talking();*/
+                    break;
+            }
+        } while (!finish);
+    }
+}
